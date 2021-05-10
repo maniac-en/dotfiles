6d48420ea3cfbba4a03c930e54386b09b84a779d
maniac <45049766+maniac-en@users.noreply.github.com>
2021-05-11 03:29:44 +0530

    Minor change 3.5
    - attach fixed terminal only to `0x11` if available
    - changing function name _git -> _git_status as it interfares with git's
      zsh completions

--- zsh/.zsh/zsh-config/35-prompt.zsh
+++ zsh/.zsh/zsh-config/35-prompt.zsh
@@ -8,7 +8,7 @@ function _short_dir() {
 # check is worktree is dirty or clean using git status --porcelain (link below)
 # https://stackoverflow.com/questions/19206816/what-to-add-to-git-status-porcelain-to-make-it-behave-like-git-status
 # echo current branch name with red as dirty branch and white as clean branch
-function _git() {
+function _git_status() {
 	if git rev-parse --git-dir > /dev/null 2>&1
 	then
 		local branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')
@@ -23,4 +23,4 @@ function _git() {
 	fi
 }
 
-PS1='%F{214}$(_short_dir)$(_git) %F{white}'
+PS1='%F{214}$(_short_dir)$(_git_status) %F{white}'
