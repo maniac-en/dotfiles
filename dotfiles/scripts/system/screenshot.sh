c4bac707e623293ead83e8204dde2a90403221fa
Bhavye Malhotra <bhavyemalhotra67@gmail.com>
2021-05-18 02:24:35 +0530

    Patching screenshot.sh
        Initially the script gave error while saving the file under
        same minute. This patch should fix the issue. I have also
        changed the default logfile name and method.

--- dotfiles/scripts/system/screenshot.sh
+++ dotfiles/scripts/system/screenshot.sh
@@ -1,38 +1,63 @@
 #!/usr/bin/env bash
 
 if ! hash maim 2>/dev/null; then exit 1; fi
+
 API_KEY="$IMGUR_CLIENT_SECRET"
+SCREENSHOT_DIR="$HOME/screenshots"
+LOG_FILE="$HOME/.cache/screenshots.log"
+new_img_path=""
+new_img_name=""
+
+mkdir -p "$SCREENSHOT_DIR"
+img_path=$(mktemp --suffix .png -p "$SCREENSHOT_DIR")
 
 function _save_log() {
-	echo "$(date): $1 : $2" >> "$HOME"/.cache/imgur_links.log
+    echo "$(date): $1" >> "$LOG_FILE"
 }
 
-function _take_screenshot() {
-	img_path=$(mktemp --suffix .png -p "$HOME"/screenshots/)
-	if [[ "$1" == "-partial" ]]
-	then
-		maim -m 5 -s "$img_path"
-		[[ "$?" -ne 0 ]] && rm -f "$img_path" && exit 1
-	elif [[ "$1" == "-full" ]]
-	then
-		maim -m 5 "$img_path" || exit 1
-	else
-		rm "$img_path"
-		exit 1
-	fi
-	if [[ "$2" == "offline" ]]
-	then
-		new_img_path=$(dirname $img_path)/$(date +"%d-%m-%Y_%R").png
-		mv "$img_path" "$new_img_path"
-		notify2 "Screenshot.sh" "Offline screenshot saved\n$new_img_path"
-		_save_log "$new_img_path" "OFFLINE" || exit 1
-	else
-		link=$(curl -s -X POST -H "Authorization: Client-ID $API_KEY" -F "image=@$img_path" https://api.imgur.com/3/upload.xml | grep -oE '<link>([^<]+)</link>' | sed 's#</\?link>##g')
-		[ -z "$link" ] && \
-			notify2 "Screenshot.sh" "Upload failed!\nImage name: $(basename $img_path)" &&  _save_log "$(basename $img_path)" "No LINK" && exit 1 \
-			|| (_save_log "$(basename $img_path)" "$link" && rm -f "$img_path")
-					(echo -n "$link" | xclip -selection clipboard) || exit 1
-	fi
+function _save_offline() {
+    cd "$SCREENSHOT_DIR"
+
+    latest_image_index=$(for image_name in *.png; do echo $image_name | grep -F "screenshot"; \
+    done 2>/dev/null | sort -V | tail -n1 | awk -F'.' '{print substr($0,11,length($0)-14)}';)
+
+    [[ -z "$latest_image_index" ]] && latest_image_index=0
+    new_index=$((latest_image_index+1))
+    new_img_name="screenshot$new_index.png"
+    new_img_path="$(dirname "$1")/$new_img_name"
+    mv "$1" "$new_img_path"
+    cd "$OLDPWD"
 }
 
-_take_screenshot "$@"
+function _exit_now() {
+    rm -f "$img_path" && exit 1
+}
+
+if [[ "$1" == "-partial" ]]
+then
+    if ! (maim -m 5 -s "$img_path"); then
+        notify2 "Screenshot.sh" "There was an issue taking a screenshot" && _exit_now
+    fi
+elif [[ "$1" == "-full" ]]
+then
+    if ! (maim -m 5 "$img_path"); then
+        notify2 "Screenshot.sh" "There was an issue taking a screenshot" && _exit_now
+    fi
+else
+    _exit_now
+fi
+
+if [[ "$2" == "offline" ]]
+then
+    (_save_offline "$img_path" && _save_log "$new_img_name" \
+        && notify2 "Screenshot.sh" "Offline screenshot saved:\n$new_img_name" ) || exit 1
+else
+
+    link=$(curl -s -X POST -H "Authorization: Client-ID $API_KEY" \
+	    -F "image=@$img_path" https://api.imgur.com/3/upload.xml \
+	    | grep -oE '<link>([^<]+)</link>' | sed 's#</\?link>##g')
+
+    ([[ -z "$link" ]] && _save_offline "$img_path" && _save_log "$new_img_name" \
+        && notify2 "Screenshot.sh" "Upload failed!\nImage saved offline: $new_img_name") \
+        || (_save_log "$link" && xclip -selection clipboard <<< "$link" && rm -f "$img_path") || exit 1
+fi
