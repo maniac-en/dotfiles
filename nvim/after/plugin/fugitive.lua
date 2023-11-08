e237e6a408f6ecab38dc086332117f655e1977b2
Shivam Mehta <sm.cse17@gmail.com>
2023-11-08 12:44:41 +0530

    better git browsing

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -4,7 +4,8 @@ vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>gd', ':Gvdiffsplit<SPACE>', { noremap = true })
 -- open current git file in browser
 -- for github -> https://github.com/tpope/vim-rhubarb
-vim.keymap.set('n', '<leader>gb', ':GBrowse<CR>', { noremap = true })
+-- vim.keymap.set('n', '<leader>gb', ':GBrowse<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>gb', ':GBrowse % @', { noremap = true })
 -- write to git work tree and index versions of the file
 vim.keymap.set('n', '<leader>gw', ':Gwrite<CR>', { noremap = true })
 -- git signed verbose commit with unified diff
