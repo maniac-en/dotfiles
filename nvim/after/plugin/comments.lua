2cad70d97d40ae82835364623f5ee9659267dd37
Shivam Mehta <sm.cse17@gmail.com>
2023-11-08 13:33:11 +0530

    setting up commenting

--- nvim/after/plugin/comments.lua
+++ nvim/after/plugin/comments.lua
@@ -1,3 +1,10 @@
+require("Comment").setup({
+  mappings = {
+    basic = true,
+    extra = true,
+  }
+})
+
 local comment_ft = require("Comment.ft")
 
 -- add an extra space whilst commenting in lua
